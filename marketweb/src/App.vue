267bee777e78512bcbda408faeb5b32e606a3bda
AmyOrz <340606700@qq.com>
2017-06-23 09:53:49 +0800

    xm[D

--- marketweb/src/App.vue
+++ marketweb/src/App.vue
@@ -16,6 +16,7 @@ export default {
   },
   components: { vheader },
   created () {
+
     this.$store.state.token = window.localStorage["token"];
     alert("App "+this.$store.state.token )
     if (this.$store.state.token == '' || this.$store.state.token == void 0) {
